ca631b5b04e0923a537987c61c37ec6efb581a9b
Na-asai <b98911218@gmail.com>
2022-04-17 11:41:54 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_04_17_003000) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
